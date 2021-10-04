1b9d45c332cda611c011260a0fe9c116ca0eff0f
DiegoBoyaca <dboyacafuquen24@gmail.com>
2021-10-04 00:29:53 -0500

    Modificación Setting.py con conexión BD

--- alimentosTropicalesProject/settings.py
+++ alimentosTropicalesProject/settings.py
@@ -98,12 +98,8 @@ WSGI_APPLICATION = 'alimentosTropicalesProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql-psycopg2',
-        'NAME' : 'p8gp6-Alimentos_Tropicales',
-        'USER' : 'postgress',
-        'PASSWORD' : 'postgress',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
