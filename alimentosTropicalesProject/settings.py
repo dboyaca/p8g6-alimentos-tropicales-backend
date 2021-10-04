f756f2332b7c623b118b620806614e3a799c4abd
DiegoBoyaca <dboyacafuquen24@gmail.com>
2021-10-03 23:20:53 -0500

    Modificación settings

--- alimentosTropicalesProject/settings.py
+++ alimentosTropicalesProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'consultaAlimentosApp',
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME' : timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS' : False,
+    'BLACKLIST_AFTER_ROTATION' : True,
+    'UPDATE_LAST_LOGIN' : False,
+    'ALGORITHM' : 'HS256',
+    'USER_ID_FIELD' : 'id',
+    'USER_ID_CLAIM' : 'user_id'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +62,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permission.AllowAny',
+    ),
+    'DEFAULT AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+    )
+}
 
 ROOT_URLCONF = 'alimentosTropicalesProject.urls'
 
@@ -75,8 +98,12 @@ WSGI_APPLICATION = 'alimentosTropicalesProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql-psycopg2',
+        'NAME' : 'p8gp6-Alimentos_Tropicales',
+        'USER' : 'postgress',
+        'PASSWORD' : 'postgress',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
