def69b747bbd386b3f68a09dced0aceb596b1053
DiegoBoyaca <dboyacafuquen24@gmail.com>
2021-10-12 09:55:27 -0500

    Creación de la vista CrearCultivo

---
+++ consultaAlimentosApp/serializers/alimentoTropicalSerializers.py
@@ -0,0 +1,42 @@
+from consultaAlimentosApp.models.alimentoTropical   import AlimentoTropical
+from rest_framework                                 import serializers
+
+class AlimentoTropicalSerializer(serializers.ModelSerializer):
+
+    """--Esta parte me traduce de JSON a Modelo"""
+    class Meta: 
+        model = AlimentoTropical
+        fields = ['id', 'nombre', 'pesoPromUnidad','grasaPromedio','proteinaPromedio',
+                'carboPromedio','vitaminaC','vitaminaD','hierro']
+
+
+
+    """--Acá me traduce del modelo al JSON"""
+    """--obj: Objeto del modelo a serializar"""
+    def to_representation(self, obj):
+        
+        alimentoTropical = AlimentoTropical.objects.get(id=obj.id)
+
+        return {
+            "id"                : alimentoTropical.id,
+            "nombre"            : alimentoTropical.nombre,
+            "pesoPromUnidad"    : alimentoTropical.pesoPromUnidad,
+            "grasaPromedio"     : alimentoTropical.grasaPromedio,
+            "proteinaPromedio"  : alimentoTropical.proteinaPrmedio,
+            "carboPromedio"     : alimentoTropical.carboPromedio,
+            "vitminaC"          : alimentoTropical.vitaminaC,
+            "vitaminaD"         : alimentoTropical.vitaminaD,
+            "hierro"            : alimentoTropical.hierro
+        }
+
+
+
+            
+
+
+
+
+
+
+
+
---
+++ consultaAlimentosApp/serializers/cultivoSerializers.py
@@ -0,0 +1,72 @@
+from rest_framework                                 import serializers
+
+from consultaAlimentosApp.models.cultivo            import Cultivo
+from consultaAlimentosApp.models.alimentoTropical   import AlimentoTropical
+from consultaAlimentosApp.models.zona               import Zona
+
+
+
+from django.db                                      import connection
+
+
+#---------------------------------------------------------------------------------
+#                        Funciones para consulta pura SQL        
+#---------------------------------------------------------------------------------
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+"""Query diseñado para traer el pais y la región en la que está establecida un cultivo sin necesidad
+    de pasar por la tabla zona"""
+def custom_sql(idCultivo):
+    with connection.cursor() as cursor:
+            sql = "SELECT Cultivo.id, Pais.nombre AS pais, Region.nombre AS region \
+                    FROM consultaalimentosapp_cultivo AS Cultivo \
+                    JOIN consultaalimentosapp_zona AS Zona ON Cultivo.zonaCultivo_id = Zona.id \
+                    JOIN consultaalimentosapp_pais AS Pais ON Zona.id_pais_id = Pais.id  \
+                    JOIN consultaalimentosapp_region AS Region ON Zona.id_region_id = Region.id \
+                    WHERE Cultivo.id = %s;"
+            cursor.execute(sql, ((idCultivo), ))
+            resultSet = dictfetchall(cursor) # [{'id': 9, 'pais': 'Colombia', 'region': 'Boyacá'}]
+    return resultSet[0]
+
+#---------------------------------------------------------------------------------
+#                               CultivoSerializer      
+#---------------------------------------------------------------------------------
+
+class CultivoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cultivo
+        fields = ['id', 'alimentoSembrado', 'hectareas','fecha','zonaCultivo']
+
+    def to_representation(self, obj):
+        cultivo = Cultivo.objects.get(id = obj.id)
+        alimento_tropical = AlimentoTropical.objects.get(id=obj.alimentoSembrado)
+        datosZona = custom_sql(obj.id)
+
+        return {
+            'id'            : cultivo.id,
+            'alimento_sembrado' : {
+                'nombre'        : alimento_tropical.nombre,
+                'proteina'      : alimento_tropical.proteinaPromedio,
+                'grasa'         : alimento_tropical.grasaPromedio,
+                'carbohidratos' : alimento_tropical.carboPromedio,
+            },         
+            'hectareas'     : cultivo.hectareas,
+            'fecha'         : cultivo.fecha,
+            'zonaCultivo'       : {
+                'pais'          : datosZona['pais'],
+                'region'        : datosZona['region']
+            }
+        }
+
+
+
+        
+
+        
